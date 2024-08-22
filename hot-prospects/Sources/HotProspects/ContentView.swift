50f518309354454d42e16fd2692a6f78c06f582b
Taarna <ivana.rast@gmail.com>
2024-08-22 12:24:50 +0200

    Hot Prospects

---
+++ hot-prospects/Sources/HotProspects/ContentView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+public struct ContentView: View {
+    @State private var selectedTab = "One"
+    
+    public init() { }
+    
+    public var body: some View {
+        TabView(selection: $selectedTab) {
+            Button("Show Tab 2") {
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            .tag("One")
+            
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
+    }
+}
