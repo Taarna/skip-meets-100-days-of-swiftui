fd93a2b8df0c46276959743aafedd029cb3a22a9
Taarna <ivana.rast@gmail.com>
2024-08-22 15:19:42 +0200

    Day 80 - part 2

--- hot-prospects/Sources/HotProspects/ContentView.swift
+++ hot-prospects/Sources/HotProspects/ContentView.swift
@@ -1,28 +1,11 @@
 import SwiftUI
 
 public struct ContentView: View {
-    @State private var output = ""
-
     public var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-
-    func fetchReadings() async {
-        let fetchTask = Task {
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        do {
-            let result = try await fetchTask.value
-            output = result
-        } catch {
-            output = "Error: \(error.localizedDescription)"
-        }
+        Image("example", bundle: .module, label: Text("Alien"))
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .background(.black)
     }
 }
